e6714f23a45e9f617fca9a8d0315ddc6f4ebcf25
alanbright <bae0497@gmail.com>
2017-10-03 14:44:59 +0800

    Update my_answer.m

--- ex1/my_answer.m
+++ ex1/my_answer.m
@@ -1,2 +1,11 @@
-// cost function
+% cost function
 J = sum(( X * theta - y ) .^2 ) / (2 * m)
+
+% gradient descent
+theta = theta - (alpha /m) * (( X * theta - y )' * X)';
+
+% this is more simplier
+theta = theta - (alpha /m) * X'( X * theta - y );
+
+% feature normalize
+X_norm = (X - mean(X)) ./ std(X)
