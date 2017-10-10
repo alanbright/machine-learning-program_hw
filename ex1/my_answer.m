58c4a0664714e2cdd0d2a3dbe3797e66fc697725
alanbright <bae0497@gmail.com>
2017-10-10 14:45:46 +0800

    Update my_answer.m

--- ex1/my_answer.m
+++ ex1/my_answer.m
@@ -5,7 +5,7 @@ J = sum(( X * theta - y ) .^2 ) / (2 * m)
 theta = theta - (alpha /m) * (( X * theta - y )' * X)';
 
 % this is more simplier
-theta = theta - (alpha /m) * X'( X * theta - y );
+theta = theta - (alpha /m) * X' * ( X * theta - y );
 
 % feature normalize
 X_norm = (X - mean(X)) ./ std(X)
