02057ae58dca114ce9c4b3172b498bf3d17d116f
alanbright <bae0497@gmail.com>
2017-10-10 14:56:59 +0800

    Create predictOneVsAll.m

---
+++ ex3/predictOneVsAll.m
@@ -0,0 +1,16 @@
+%X has size 5000 x 400
+%Theta1 has size 25 x 401
+%Theta2 has size 10 x 26
+
+% Add ones to the X data matrix
+X = [ones(m, 1) X]; % 5000 x 401
+
+%get z2 then get A2
+z2 = X * Theta1' ;
+A2 = sigmoid(z2); % 5000 x 25
+
+% Add ones to the A2 matrix
+A2 = [ones(m, 1) A2]; % 5000 x 26
+
+[Ans, AnsIndex] = max(A2 * Theta2' , [] ,2);
+p = AnsIndex;
